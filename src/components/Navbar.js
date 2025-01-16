90bf9afea7bd34fce919f2c6fb3c8c60f5f91f6a
Shtakshi Sharma <148087057+shtakshi8055@users.noreply.github.com>
2025-01-16 22:09:49 +0530

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
@@ -92,7 +90,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link
                 href="https://my-porfolio-sage.vercel.app/"
                 target="_blank"
@@ -100,7 +98,7 @@ function NavBar() {
               >
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
 
             <Nav.Item className="fork-btn">
               <Button
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ import Ecommerce from "../../Assets/Projects/Ecommerce.png";
 // import suicide from "../../Assets/Projects/suicide.png";
 import todolist from "../../Assets/Projects/todolist.png";
 import mindsweeper from "../../Assets/Projects/mindsweeper.png";
-
+import mood from "../../Assets/Projects/mood.png"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -51,7 +51,7 @@ function Projects() {
              // demoLink="https://to-do-list-swart-beta-93.vercel.app/"
             />
           </Col>
-          <Col md={4} className="project-card">
+          {/* <Col md={4} className="project-card">
             <ProjectCard
               imgPath={teamCollab}
               isBlog={false}
@@ -59,6 +59,16 @@ function Projects() {
               description=" This Team Management and Task Assignment app is designed to streamline collaboration within teams. Built using React.js and Firebase, it allows users to create teams, assign tasks, and manage workflows efficiently. The real-time synchronization ensures that all team members are kept up-to-date with task progress. The app features intuitive interfaces for creating teams, managing members, and assigning tasks, making it an ideal tool for teams of all sizes looking to improve their project management and collaboration. Whether you’re managing a small team or handling multiple projects, this app simplifies the entire process."
               ghLink="https://github.com/shtakshi8055/Team-Collab"
               demoLink="https://team-collab-rust.vercel.app/"
+            />
+          </Col> */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={mood}
+              isBlog={false}
+              title="Mood-Tracker"
+              description=" A Mood Tracking App built using Next.js that allows users to log and monitor their daily emotional states. The app provides an intuitive interface for users to track their mood over time, visualize trends, and reflect on their emotional well-being. The app emphasizes a simple, user-friendly design and utilizes modern web technologies to ensure a smooth and responsive experience."
+              ghLink="https://github.com/shtakshi8055/Broodlee"
+              demoLink="https://broodlenew.vercel.app/"
             />
           </Col>
         </Row>
